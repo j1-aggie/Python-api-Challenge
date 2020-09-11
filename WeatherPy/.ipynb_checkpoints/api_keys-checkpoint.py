5574db2615dba82e4496d0c1afe303c1bf6b9f71
Jeremy jones <j1-aggie@hotmail.com>
2020-09-11 14:00:41 -0500

    saving

--- WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -2,4 +2,4 @@
 weather_api_key = "486a3f0a342100df108bae3ce4d66f0e"
                    
 # Google API Key
-#g_key = "AIzaSyCYb0jNY3OgTKmwyHDqVvlfZe14ybLRQ3k"
+g_key = "AIzaSyCYb0jNY3OgTKmwyHDqVvlfZe14ybLRQ3k"
