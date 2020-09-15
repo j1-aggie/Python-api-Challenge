5626dfccbd83fe4af4116e3f0af43a98574e44e9
Jeremy jones <j1-aggie@hotmail.com>
2020-09-15 14:05:14 -0500

    saving

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "486a3f0a342100df108bae3ce4d66f0e"
                    
 # Google API Key
-g_key = "AIzaSyB8zIxYK9cDv6VqtnJxDH1c2C5rAHhWAVE"
+g_key = "AIzaSyBHSEEzO1wTmfLvQDBuTnbpkDE8uGALtu4"
